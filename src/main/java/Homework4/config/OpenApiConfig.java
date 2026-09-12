42bbcbb1408b9a2f0f8a192c4b4bd015456532e4
ivankavaleuski <vn.kvlvsk@gmail.com>
2026-09-12 20:29:47 +0300

    homework6 added

---
+++ src/main/java/Homework4/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package Homework4.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI userServiceOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("User Service API")
+                        .description("CRUD API для управления пользователями (Homework4)")
+                        .version("v1"));
+    }
+}
