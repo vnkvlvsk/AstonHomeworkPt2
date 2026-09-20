10d4692d29b80ca7917e85052520ee3fb82f2b5f
ivankavaleuski <vn.kvlvsk@gmail.com>
2026-09-20 23:08:17 +0300

    homework7 added

---
+++ src/main/java/DiscoveryServer/DiscoveryServerApplication.java
@@ -0,0 +1,23 @@
+package DiscoveryServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
+public class DiscoveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(DiscoveryServerApplication.class);
+        app.setAdditionalProfiles("discovery-server");
+        app.run(args);
+    }
+}
